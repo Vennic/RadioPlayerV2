2de83b3647d07a14ea936fa65ec35e3d819e2e0e
Adel Kuzheev <kuzheevadel@gmail.com>
2021-07-12 22:53:14 +0500

    Add fragments layouts

--- app/src/main/java/com/kuzheevadel/radioplayerv2/activities/main/MainActivity.kt
+++ app/src/main/java/com/kuzheevadel/radioplayerv2/activities/main/MainActivity.kt
@@ -2,6 +2,9 @@ package com.kuzheevadel.radioplayerv2.activities.main
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.viewpager2.widget.ViewPager2
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
 import com.kuzheevadel.radioplayerv2.R
 
 class MainActivity: AppCompatActivity() {
@@ -9,5 +12,17 @@ class MainActivity: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val viewPager = findViewById<ViewPager2>(R.id.view_pager2)
+        val tabLayout = findViewById<TabLayout>(R.id.tab_layout)
+        viewPager.adapter = ViewPagerAdapter(this)
+
+        TabLayoutMediator(tabLayout, viewPager) { tab, position ->
+            when (position) {
+                0 -> tab.text = "All tracks"
+                1 -> tab.text = "Albums"
+                2 -> tab.text = "Playlist"
+                else -> tab.text = "Radio"
+            }
+        }.attach()
     }
 }
--- app/src/main/java/com/kuzheevadel/radioplayerv2/alltracks/AllTracksFragment.kt
+++ app/src/main/java/com/kuzheevadel/radioplayerv2/alltracks/AllTracksFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.kuzheevadel.radioplayerv2.R
 
 class AllTracksFragment: Fragment() {
 
@@ -13,6 +14,6 @@ class AllTracksFragment: Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return super.onCreateView(inflater, container, savedInstanceState)
+        return inflater.inflate(R.layout.all_tracks_layout, container, false)
     }
 }
