5a7028c1768d9ea9a74152a3d2e84074fcfe2dc5
Adel Kuzheev <kuzheevadel@gmail.com>
2021-07-12 22:31:07 +0500

    Replace MainActivity

---
+++ app/src/main/java/com/kuzheevadel/radioplayerv2/activities/main/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.kuzheevadel.radioplayerv2.activities.main
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.kuzheevadel.radioplayerv2.R
+
+class MainActivity: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
