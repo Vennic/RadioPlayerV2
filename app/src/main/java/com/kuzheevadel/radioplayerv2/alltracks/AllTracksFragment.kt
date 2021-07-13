baeee43f218fbfc32f0f52ec9935ef45e9787dc7
Adel Kuzheev <kuzheevadel@gmail.com>
2021-07-13 22:02:45 +0500

    Add READ_EXTERNAL_STORAGE permission check

--- app/src/main/java/com/kuzheevadel/radioplayerv2/alltracks/AllTracksFragment.kt
+++ app/src/main/java/com/kuzheevadel/radioplayerv2/alltracks/AllTracksFragment.kt
@@ -1,17 +1,19 @@
 package com.kuzheevadel.radioplayerv2.alltracks
 
+import android.Manifest
 import android.content.Context
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
-import com.kuzheevadel.radioplayerv2.R
 import com.kuzheevadel.radioplayerv2.databinding.AllTracksLayoutBinding
 import com.kuzheevadel.radioplayerv2.di.PlayerApplication
 import javax.inject.Inject
@@ -25,6 +27,17 @@ class AllTracksFragment: Fragment() {
     lateinit var viewModelFactory: ViewModelProvider.Factory
 
     private val viewModel by viewModels<AllTracksViewModel> { viewModelFactory }
+
+    private val requestPermissionLauncher =
+        registerForActivityResult(ActivityResultContracts.RequestPermission()
+        ) {
+            isGranted: Boolean ->
+            if (isGranted) {
+                Log.d("ASDF", "Permission is granted in callback")
+            } else {
+                Log.d("ASDF", "Permission is not granted")
+            }
+        }
 
     override fun onAttach(context: Context) {
         super.onAttach(context)
@@ -57,10 +70,33 @@ class AllTracksFragment: Fragment() {
         }
 
         Log.d("ASDF", "$viewModel")
+        checkReadStoragePermission()
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    private fun checkReadStoragePermission() {
+        when {
+            ContextCompat.checkSelfPermission(
+                requireContext(),
+                Manifest.permission.READ_EXTERNAL_STORAGE
+            ) == PackageManager.PERMISSION_GRANTED -> {
+                Log.d("ASDF", "Granted in check")
+            }
+            shouldShowRequestPermissionRationale(Manifest.permission.READ_EXTERNAL_STORAGE) -> {
+                Log.d("ASDF", "Show permission rationale")
+                requestPermissionLauncher.launch(
+                    Manifest.permission.READ_EXTERNAL_STORAGE
+                )
+            }
+            else -> {
+                requestPermissionLauncher.launch(
+                    Manifest.permission.READ_EXTERNAL_STORAGE
+                )
+            }
+        }
     }
 }
